c39ec54d878922cac23520934e419fdab167e099
Ryoma <e165743@ie.u-ryukyu.ac.jp>
2017-01-13 19:25:42 +0900

    1st

---
+++ src/main/java/jp/ac/uryukyu/ie/e165743/Enemy.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e165743;
+/**
+ * Created by e165743H on 2016/11/26.
+ */
+public class Enemy extends LivingThing {
+
+    public Enemy(String name, int maximumHP, int attack){
+        super(name,maximumHP,attack);
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e165743/Hero.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165743;
+/**
+ * Created by e165743H on 2016/11/26.
+
+ */
+public class Hero extends LivingThing {
+
+    public Hero(String name, int maximumHP, int attack) {
+        super(name,maximumHP, attack);
+    }
+}
